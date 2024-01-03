58b2394d47310e3d579875632e8e1c076651023f
sachindj <sachinjadhavdn@gmail.com>
2024-01-03 14:38:56 +0530

    changes

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ res.send("hello world 123 !!!");
 
 })
 
+// post api
+app.post("/userpost", ()=>{
+    res.send("hello world 123 !!!");
+    
+    
+    })
+
 app.listen(5000, ()=>{
 console.log("app is listening on port 5000")
 
